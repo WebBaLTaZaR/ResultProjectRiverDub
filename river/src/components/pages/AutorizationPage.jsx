ca18c536672b31948ae1e4cd100734ed86edca86
GricaevSergey <gricaev_sergey@mail.ru>
2025-03-11 10:50:27 +0500

    стилизовал вкладку авторизации

--- river/src/components/pages/AutorizationPage.jsx
+++ river/src/components/pages/AutorizationPage.jsx
@@ -1,16 +1,42 @@
+import { Link } from "react-router-dom";
 import Footer from "../Footer";
 import Header from "../Header";
 
 function AutorizationPage() {
-	return ( <>
-	<div className="contentWrap">
-		<Header/>
-		<div className="autorizationPrompt">
-			<div>Авторизоваться или войти</div>
-		</div>
-		<Footer/>
-	</div>
-	</> );
+  return (
+    <>
+      <div className="contentWrap">
+        <Header />
+        <div className="autorizationPrompt">
+			<h3 className="autorizationTitle">Авторизация</h3>
+          <div>Электронная почта</div>
+          <input
+            type="email"
+            id="email"
+            pattern=".+@example\.com"
+            size="30"
+            required
+			placeholder="example@mail.ru"
+          />
+          <div>Введите пароль</div>
+          <input
+		  placeholder="password"
+            type="password"
+            id="inputPass"
+            name="password"
+            minlength="8"
+            required
+          />
+          <Link to={"/"} className="fogotPass">Забыли пароль?</Link>
+          <div className="lognWrap">
+            <button className="button butLogin">Войти</button>
+            <button className="button butSignin">Регистрация</button>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default AutorizationPage;
